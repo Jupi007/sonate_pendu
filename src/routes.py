54c5c372347d171f3c27859a2768480ac2446e7f
Paul Kepinski <paul.kepinski@gmail.com>
2026-04-09 22:56:43 +0200

    Add username form

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 routes = Blueprint("routes", __name__, template_folder="templates")
 
 
-@routes.route("/")
-def home():
-    with open("src/data/dictionnaire.txt", "r") as file:
-        word_list = [line.split(";")[0].strip() for line in file if line.strip()]
-
-    return render_template("index.html", word_list=word_list)
+@routes.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html", username=request.form.get("username"))
